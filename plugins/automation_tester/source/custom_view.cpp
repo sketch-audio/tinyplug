40d4010db56d3550c61a7358a45a0924f3773a3c
Ryan Robinson <rygrob@gmail.com>
2025-09-02 16:38:39 -0700

    Create plug-in: Automation Tester.

---
+++ plugins/automation_tester/source/custom_view.cpp
@@ -0,0 +1,94 @@
+#include "custom_view.h"
+
+#include <variant>
+
+#include "include/core/SkCanvas.h"
+#include "include/core/SkSurface.h"
+
+#include "platform/platform_view.h"
+
+namespace tiny {
+
+// MARK: - on create
+
+auto Custom_view::on_create(const Action_queue::Receiver& actions, const Task_queue::Receiver& tasks) -> void
+{
+    _actions = actions;
+    _task_receiver = tasks;
+}
+
+// MARK: - on draw
+
+auto Custom_view::on_draw(App_state& app_state) -> void
+{
+    auto& params_state = app_state.params_state;
+    auto& view_context = app_state.view_context;
+
+    auto& interaction = view_context.interaction;
+    auto* canvas = view_context.canvas;
+
+    // Resolve the real size.
+    const auto lsize = view_context.logical_size;
+    const auto scale = view_context.scale;
+    const auto rsize = Rect_size{
+        .w = static_cast<int32_t>(lsize.w * scale),
+        .h = static_cast<int32_t>(lsize.h * scale)
+    };
+
+    // Get param, export values.
+    auto& params = params_state.params;
+    auto& exports = params_state.exports;
+
+    const auto id = enum_raw(Param_id::gain);
+    const auto g = static_cast<float>(params[id]);
+
+    // Get incremented parameter value from a drag.
+    auto get_next = [=, this](auto x, auto& d) -> double {
+        const auto drag_y = d.tpos.y - d.fpos.y;
+        const auto drag_dy = drag_y - _ldrag;
+        const auto norm_dy = drag_dy / lsize.h;
+        return std::clamp(x - norm_dy, double{}, double{1});
+    };
+
+    // Handle user actions.
+    std::visit(Inline_visitor{
+        [&](const Drag_start& s) {
+            _ldrag = {};
+            const auto to_set = get_next(g, s);
+            _actions.push(Action_start{id});
+            _actions.push(Set_param{id, to_set});
+            _ldrag = s.tpos.y - s.fpos.y;
+        },
+        [&](const Drag& s) {
+            const auto to_set = get_next(g, s);
+            _actions.push(Set_param{id, to_set});
+            _ldrag = s.tpos.y - s.fpos.y;
+        },
+        [&](const Drag_end& s) {
+            const auto to_set = get_next(g, s);
+            _actions.push(Set_param{id, to_set});
+            _actions.push(Action_end{id});
+            _ldrag = {};
+        },
+        [](const auto&) {}
+    }, interaction.state);
+    track_is_down(interaction.state, _down); // Track down.
+
+    // Draw background.
+    auto paint = SkPaint{};
+    paint.setColor(view_context.dark_mode ? SK_ColorBLACK : SK_ColorWHITE);
+    paint.setStyle(SkPaint::kFill_Style);
+    canvas->drawRect(SkRect::MakeXYWH(0, 0, static_cast<float>(rsize.w), static_cast<float>(rsize.h)), paint);
+
+    // Draw gain value.
+    paint.setColor(view_context.dark_mode ? SK_ColorWHITE : SK_ColorBLACK);
+    if (_down) {
+        paint.setColor(SK_ColorBLUE);
+    }
+
+    const auto g_h = g * rsize.h;
+    const auto g_y = rsize.h - g_h;
+    canvas->drawRect(SkRect::MakeXYWH(0, g_y, static_cast<float>(rsize.w), g_h), paint);
+}
+
+} // namespace tiny
---
+++ plugins/automation_tester/source/custom_view.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include "tinyplug/tinyplug.h"
+#include "param_model.h"
+
+namespace tiny {
+
+class Custom_view {
+public:
+    // This is where your view is "created" (maybe presented is a better term).
+    // You receive some things that will persist until the next `on_create`.
+    // - Action receiver: In `on_draw` you can push user actions like `Set_param` to the host/DSP.
+    // - Task receiver: If you need a `Later` executed on the draw thread, you can use it to capture `this` and push a task.
+    auto on_create(const Action_queue::Receiver& actions, const Task_queue::Receiver& tasks) -> void;
+
+    // This is where you draw your plug-in's UI and handle the user's interaction.
+    // The app state gives you
+    // - Read-only access to the param and export values.
+    // - A view context with the interaction state and a canvas in which to draw.
+    auto on_draw(App_state& app_state) -> void;
+
+private:
+
+    using User_params = Param_infos<Param_model>;
+    using Param_id = Param_model::Param_id;
+
+    User_params _params{};
+    Action_queue::Receiver _actions{};
+    Task_queue::Receiver _task_receiver{};
+
+    bool _down{};
+    double _ldrag{}; // norm
+
+};
+
+} // namespace tiny
