8c65bbd97ad8588960ce11407a1637a714a3f04e
Ryan Robinson <rygrob@gmail.com>
2026-07-24 14:07:17 -0700

    Look up bundle by identifier.

--- libs/tiny_platform/source/ios_paths.mm
+++ libs/tiny_platform/source/ios_paths.mm
@@ -1,18 +1,35 @@
 #include <tiny_platform/platform_paths.hpp>
 
+#include <CoreFoundation/CoreFoundation.h>
 #include <Foundation/Foundation.h>
+
+#include "tinyplug/tinyplug.hpp"
+
+#include <array>
+#include <climits>
 
 namespace tiny {
 
-auto Platform_paths::format_readable(const std::string& /*bundle_id*/) -> std::filesystem::path
+auto Platform_paths::format_readable(const std::string& bundle_id) -> std::filesystem::path
 {
-    // On iOS we have our own bundle.
-    auto bundle = [NSBundle mainBundle];
-    auto resources = [bundle resourcePath];
-    if (!resources) return {};
-    
-    const auto path = std::filesystem::path{[resources UTF8String]};
-    return path;
+    // Look up by bundle identifier. Succeeds in both standlone and extension.
+    const auto bundle_str = CFStringCreateWithCString(kCFAllocatorDefault, bundle_id.c_str(), kCFStringEncodingUTF8);
+    const auto release_bundle_str = Deferred{[&]() { if (bundle_str) CFRelease(bundle_str); }};
+
+    const auto bundle = CFBundleGetBundleWithIdentifier(bundle_str);
+    if (!bundle) return {};
+
+    const auto resources_url = CFBundleCopyResourcesDirectoryURL(bundle);
+    const auto release_resources_url = Deferred{[&]() { if (resources_url) CFRelease(resources_url); }};
+
+    auto buffer = std::array<char, PATH_MAX>{};
+    auto data = reinterpret_cast<uint8_t*>(buffer.data());
+    auto size = static_cast<CFIndex>(buffer.size());
+    const auto result = CFURLGetFileSystemRepresentation(resources_url, true, data, size);
+    if (!result) return {};
+
+    const auto bundle_path = std::filesystem::path{buffer.data()};
+    return bundle_path;
 }
 
 auto Platform_paths::shared_writable(const Subpath& subpath) -> std::filesystem::path
